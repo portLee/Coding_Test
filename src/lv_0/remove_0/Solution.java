750b3ca2af9425e9c72decbc2f96096768f89497
portLee <hungu32@gmail.com>
2024-03-15 17:26:12 +0900

    0떼기

---
+++ src/lv_0/remove_0/Solution.java
@@ -0,0 +1,21 @@
+package lv_0.remove_0;
+
+public class Solution {
+    /* 0떼기 */
+    public String solution(String n_str) {
+        Integer s_num = Integer.parseInt(n_str);
+        String answer = "";
+
+        answer = s_num.toString();
+
+        return answer;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String answer = solution.solution("854020");
+        System.out.println(answer);
+    }
+}
