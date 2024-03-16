4b94c5be6f112c3aeda3eb3bc40aecdbc69bded8
portLee <hungu32@gmail.com>
2024-03-16 16:54:55 +0900

    배열 중앙값 구하기

---
+++ src/lv_0/Find_the_median/Solution.java
@@ -0,0 +1,33 @@
+package lv_0.Find_the_median;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int index = array.length / 2;
+
+        for(int i = 0; i < array.length - 1; i++) {
+            for(int j = i + 1; j < array.length; j++) {
+                if(array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        answer = array[index];
+
+        return answer;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        int[] array = {9, -1, 0};
+        Solution solution = new Solution();
+        int answer = solution.solution(array);
+        System.out.println(answer);
+    }
+}
