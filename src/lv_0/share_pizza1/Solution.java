6057c5e0fb3556f9f48d1c345476bb2e4e0bb239
portLee <hungu32@gmail.com>
2024-03-18 16:35:07 +0900

    피자 나눠 먹기(1)

---
+++ src/lv_0/share_pizza1/Solution.java
@@ -0,0 +1,15 @@
+package lv_0.share_pizza1;
+
+public class Solution {
+    public int solution(int n) {
+        return (n % 7 == 0) ? n / 7 : n / 7 + 1;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int answer = solution.solution(3);
+        System.out.println("result = " + answer);
+    }
+}
