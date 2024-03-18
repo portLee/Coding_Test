599d85392dae2f4ef79c88ab8c41a06b477e01a9
portLee <hungu32@gmail.com>
2024-03-18 15:57:40 +0900

    최빈값 구하기

---
+++ src/lv_0/Find_the_mode/Solution.java
@@ -0,0 +1,39 @@
+package lv_0.Find_the_mode;
+
+import java.util.Map;
+
+public class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int mode = 0;
+        int modeCnt = 0;
+
+
+        for (int num : array) {
+            int count = 0;
+            for (int num2 : array) {
+                if (num == num2) {
+                    count++;
+                }
+            }
+            if (count > modeCnt) {
+                mode = num;
+                modeCnt = count;
+                answer = mode;
+            } else if (count == modeCnt && mode != num) {
+                answer = -1;
+            }
+        }
+
+        return answer;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        int[] array = {1, 1, 2, 2};
+        Solution solution = new Solution();
+        int answer = solution.solution(array);
+        System.out.println(answer);
+    }
+}
