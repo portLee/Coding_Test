1a09d4da07626907e24a1a1d5f28b166a309ac60
portLee <hungu32@gmail.com>
2024-03-19 17:17:23 +0900

    과일 장수

---
+++ src/lv_1/fruiterer/Solution.java
@@ -0,0 +1,26 @@
+package lv_1.fruiterer;
+
+import java.util.*;
+
+public class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Integer[] temp = Arrays.stream(score).boxed().toArray(Integer[]::new);
+        Arrays.sort(temp, Collections.reverseOrder());
+
+        for (int i = m - 1; i < temp.length; i += m) {
+            answer += temp[i] * m;
+        }
+
+        return answer;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        int[] score = {4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2};
+        Solution solution = new Solution();
+        int answer = solution.solution(4, 3, score);
+        System.out.println("result = " + answer);
+    }
+}
